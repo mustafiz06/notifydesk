3c739c0c1b0c5df1aaea51a81684f738a6038aea
mustafiz06 <themustafiz06@gmail.com>
2026-03-08 22:57:35 +0600

    delte home page & route

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-
 Auth::routes();
-
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/', function () {
